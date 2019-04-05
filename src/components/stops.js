7d3ca1d9de4370fa2e3fb56d5671986a2b5dfb3b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-05 14:43:31 +0200

    outsourced stop list

---
+++ src/components/stops.js
@@ -0,0 +1,23 @@
+const stops = [
+  { id: "900000160541", name: "Josef-Orlopp-Str./Vulkanstr." },
+  { id: "900000160509", name: "Bernhard-Bästlein-Str." },
+  { id: "900000160014", name: "Möllendorffstr./Storkower Str." },
+  { id: "900000012101", name: "S Anhalter Bahnhof" },
+  { id: "900000066102", name: "S Botanischer Garten" },
+  { id: "900000063101", name: "S Feuerbachstr." },
+  { id: "900000060101", name: "S Friedenau" },
+  { id: "900000057104", name: "S Julius-Leber-Brücke" },
+  { id: "900000066101", name: "S Lichterfelde West" },
+  { id: "900000050301", name: "S Mexikoplatz" },
+  { id: "900000052201", name: "S Nikolassee" },
+  { id: "900000100020", name: "S+U Potsdamer Platz" },
+  { id: "900000062202", name: "S+U Rathaus Steglitz" },
+  { id: "900000050355", name: "S Schlachtensee" },
+  { id: "900000054104", name: "S Schöneberg" },
+  { id: "900000049202", name: "S Sundgauer Str." },
+  { id: "900000053301", name: "S Wannsee" },
+  { id: "900000057103", name: "S+U Yorckstr." },
+  { id: "900000049201", name: "S Zehlendorf" }
+];
+
+export default stops;
