4e1f924275386fbfa3d01e3eace64ad87812789d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-15 17:04:20 +0200

    added stops of 190

--- src/components/stops.js
+++ src/components/stops.js
@@ -525,6 +525,7 @@ const stops = [
   { id: "900000176512", name: "Hultschiner Damm/Seestr." },
   { id: "900000074206", name: "Im Domstift/Groß-Ziethener Str." },
   { id: "900000027352", name: "Im Spektefeld/Schulzentrum" },
+  { id: "900000180023", name: "Innovationspark Wuhlheide" },
   { id: "900000060104", name: "Insulaner" },
   { id: "900000100503", name: "Invalidenpark" },
   { id: "900000092101", name: "Invalidensiedlung" },
