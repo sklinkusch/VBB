7d0b7723287239fbe480c72154df845e684a38ea
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-21 15:53:33 +0200

    filled gaps of M49

--- src/data/TBBerlin/stopsJ.js
+++ src/data/TBBerlin/stopsJ.js
@@ -5,6 +5,7 @@ const stopsJ = [
   { id: "900000023173", name: "Jebensstr." },
   { id: "900000175520", name: "Jenaer Str." },
   { id: "900000100527", name: "Jerusalemer Str." },
+  { id: "900000023206", name: "Joachimsthaler Str./Kantstr." },
   { id: "900000027201", name: "Johannesstift" },
   { id: "900000082105", name: "Johannisthaler Chaussee/Fritz-Erler-Allee" },
   { id: "900000082291", name: "Johannisthaler Chaussee/Rudower Str." },
