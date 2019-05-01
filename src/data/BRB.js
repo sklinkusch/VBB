cf39af2dd96c0bc944b9160bdadfabf1c20635cd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-01 23:05:31 +0200

    added further parts of BRB (now completed)

--- src/data/BRB.js
+++ src/data/BRB.js
@@ -1,7 +1,14 @@
 import stopsUnsorted from "./unsorted";
 
-const stops = stopsUnsorted.filter(stop =>
-  stop.name.startsWith("Brandenburg,")
+const stops = stopsUnsorted.filter(
+  stop =>
+    stop.name.startsWith("Brandenburg,") ||
+    stop.name.startsWith("Gollwitz (bei Jeserig)") ||
+    stop.name.startsWith("Göttin (Brandenburg)") ||
+    stop.name.startsWith("Klein Kreutz") ||
+    stop.name.startsWith("Saaringen,") ||
+    stop.name === "Schmerzke" ||
+    stop.name.startsWith("Wust,")
 );
 
 export { stops };
