f165eb7e24652c108df5ef861dab221b341972c2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-22 13:23:15 +0200

    replaced fontawesome from cdn with node_module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
