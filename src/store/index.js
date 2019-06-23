54618b8e7310b357cfb2114da10bc40f471d86fd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-23 17:24:29 +0200

    removed reduxDevTools for production version

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,17 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware/*, compose*/ } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { watcherSaga } from "../sagas";
 import { reducer } from "../reducers";
 
 const sagaMiddleware = createSagaMiddleware();
-const reduxDevtools =
+/*const reduxDevtools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const composed = compose(
   applyMiddleware(sagaMiddleware),
   reduxDevtools
-);
+);*/
 
-export const store = createStore(reducer, composed);
+//export const store = createStore(reducer, composed);
+export const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(watcherSaga);
