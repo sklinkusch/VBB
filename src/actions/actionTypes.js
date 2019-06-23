01376fea698567d58507e4f1693224b7eb5c0de8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-23 10:07:34 +0200

    wrote action file

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const FILTER_STOPS = "FILTER_STOPS";
+export const VBB_REQUEST = "VBB_REQUEST";
+export const VBB_SUCCESS = "VBB_SUCCESS";
+export const VBB_FAILURE = "VBB_FAILURE";
+export const FILTER_AND = "FILTER_AND";
+export const FILTER_OR = "FILTER_OR";
