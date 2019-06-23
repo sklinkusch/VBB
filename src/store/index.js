f92aa4cf05fa628665f5adcb5755ba530c9ccefa
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-23 13:22:10 +0200

    created store file

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { watcherSaga } from "../sagas";
+import { reducer } from "../reducers";
+
+const sagaMiddleware = createSagaMiddleware();
+const reduxDevtools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composed = compose(
+  applyMiddleware(sagaMiddleware),
+  reduxDevtools
+);
+
+export const store = createStore(reducer, composed);
+
+sagaMiddleware.run(watcherSaga);
