08608ee4b49f4e4706cc0dc7601d955c2f626786
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-23 10:57:48 +0200

    wrote first reducer

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -4,3 +4,4 @@ export const VBB_SUCCESS = "VBB_SUCCESS";
 export const VBB_FAILURE = "VBB_FAILURE";
 export const FILTER_AND = "FILTER_AND";
 export const FILTER_OR = "FILTER_OR";
+export const HANDLE_CHANGE = "HANDLE_CHANGE";
