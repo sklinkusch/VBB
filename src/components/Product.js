c8ed86e7bae6790d2d746795e79f0be17bd74890
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-13 14:37:21 +0200

    refactor Product as a functional component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import express from "../images/express.svg";
 import regional from "../images/regional.svg";
 import suburban from "../images/suburban.svg";
@@ -12,87 +12,86 @@ import specialbus from "../images/special-bus.svg";
 import ferry from "../images/ferry.svg";
 import "../styles/Product.scss";
 
-export default class Product extends Component {
-  render() {
-    const product = this.props.product;
-    const line = this.props.line;
-    switch (product) {
-      case "express":
+const Product = props => {
+  const { product, line } = props;
+  switch (product) {
+    case "express":
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={express} alt="express" />
+        </div>
+      );
+    case "regional":
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={regional} alt="regional" />
+        </div>
+      );
+    case "suburban":
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={suburban} alt="suburban" />
+        </div>
+      );
+    case "subway":
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={subway} alt="subway" />
+        </div>
+      );
+    case "tram":
+      if (line.startsWith("M")) {
         return (
           <div className="mean col-md-1 col-2">
-            <img src={express} alt="express" />
+            <img src={metrotram} alt="metro-tram" />
           </div>
         );
-      case "regional":
+      }
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={tram} alt="tram" />
+        </div>
+      );
+    case "bus":
+      if (line.startsWith("M")) {
         return (
           <div className="mean col-md-1 col-2">
-            <img src={regional} alt="regional" />
+            <img src={metrobus} alt="metro-bus" />
           </div>
         );
-      case "suburban":
+      }
+      if (line === "TXL" || line.startsWith("X")) {
         return (
           <div className="mean col-md-1 col-2">
-            <img src={suburban} alt="suburban" />
+            <img src={expressbus} alt="express-bus" />
           </div>
         );
-      case "subway":
+      }
+      if (line === "A05") {
         return (
           <div className="mean col-md-1 col-2">
-            <img src={subway} alt="subway" />
+            <img src={specialbus} alt="special-bus" />
           </div>
         );
-      case "tram":
-        if (line.startsWith("M")) {
-          return (
-            <div className="mean col-md-1 col-2">
-              <img src={metrotram} alt="metro-tram" />
-            </div>
-          );
-        }
-        return (
-          <div className="mean col-md-1 col-2">
-            <img src={tram} alt="tram" />
-          </div>
-        );
-      case "bus":
-        if (line.startsWith("M")) {
-          return (
-            <div className="mean col-md-1 col-2">
-              <img src={metrobus} alt="metro-bus" />
-            </div>
-          );
-        }
-        if (line === "TXL" || line.startsWith("X")) {
-          return (
-            <div className="mean col-md-1 col-2">
-              <img src={expressbus} alt="express-bus" />
-            </div>
-          );
-        }
-        if (line === "A05") {
-          return (
-            <div className="mean col-md-1 col-2">
-              <img src={specialbus} alt="special-bus" />
-            </div>
-          );
-        }
-        return (
-          <div className="mean col-md-1 col-2">
-            <img src={bus} alt="bus" />
-          </div>
-        );
-      case "ferry":
-        return (
-          <div className="mean col-md-1 col-2">
-            <img src={ferry} alt="ferry" />
-          </div>
-        );
-      default:
-        return (
-          <div className="mean col-md-1 col-2">
-            <img src={bus} alt="bus" />
-          </div>
-        );
-    }
+      }
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={bus} alt="bus" />
+        </div>
+      );
+    case "ferry":
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={ferry} alt="ferry" />
+        </div>
+      );
+    default:
+      return (
+        <div className="mean col-md-1 col-2">
+          <img src={bus} alt="bus" />
+        </div>
+      );
   }
-}
+};
+
+export default Product;
