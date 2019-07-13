5db0c2312bd81d265ed72b0342d41888f1fb1036
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-13 14:09:28 +0200

    refactor departure as a functional component

--- src/components/Departure.js
+++ src/components/Departure.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Product from "./Product";
 import "../styles/Departure.scss";
 import Barrier from "./Barrier";
@@ -9,8 +9,8 @@ import Warntext from "./Warntext";
 import Stattext from "./Stattext";
 import Time from "./Time";
 
-export default class Departure extends Component {
-  getDelay(delay, cancelled) {
+const Departure = props => {
+  const getDelay = (delay, cancelled) => {
     if (delay != null) {
       return Math.floor(delay / 60);
     } else if (cancelled) {
@@ -18,8 +18,8 @@ export default class Departure extends Component {
     } else {
       return "?";
     }
-  }
-  getPlanTime(realtime, delay) {
+  };
+  const getPlanTime = (realtime, delay) => {
     let hours = Number(realtime.substr(0, 2));
     let minutes = Number(realtime.substr(3, 2));
     minutes -= delay;
@@ -40,58 +40,56 @@ export default class Departure extends Component {
     let hourString = hours < 10 ? `0${hours}` : `${hours}`;
     let minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
     return `${hourString}:${minuteString}`;
-  }
-  getTime(timestamp) {
+  };
+  const getTime = timestamp => {
     if (timestamp != null) {
       return timestamp.substr(11, 5);
     } else {
       return "";
     }
+  };
+  let delayMin;
+  if (props.dep.cancelled) {
+    delayMin = getDelay(props.dep.delay, props.dep.cancelled);
+  } else {
+    delayMin = getDelay(props.dep.delay, false);
   }
-  render() {
-    let delayMin;
-    if (this.props.dep.cancelled) {
-      delayMin = this.getDelay(this.props.dep.delay, this.props.dep.cancelled);
-    } else {
-      delayMin = this.getDelay(this.props.dep.delay, false);
-    }
-    let realtime = this.getTime(this.props.dep.when);
-    let plantime;
-    if (this.props.dep.when != null && this.props.dep.delay != null) {
-      plantime = this.getPlanTime(realtime, delayMin);
-    } else if (this.props.dep.when != null && delayMin === "?") {
-      plantime = realtime;
-      realtime = "";
-    } else {
-      plantime =
-        this.getTime(this.props.dep.formerScheduledWhen) ||
-        this.getTime(this.props.dep.scheduledWhen);
-    }
-    const product = this.props.dep.line.product;
-    const line = this.props.dep.line.name;
-    const direction = this.props.dep.direction;
-    const remarks = this.props.dep.remarks;
-    const platform = this.props.dep.platform;
-    return (
-      <React.Fragment>
-        <div className="row" key={this.props.dep.tripId}>
-          <Time time={plantime} />
-          <Time time={realtime} />
-          <div className="delay col-md-2 col-8">{delayMin}</div>
-          <Product product={product} line={line} />
-          <div className="line col-md-3 col-6">{line}</div>
-          <div className="direction col-md-8 col-13">{direction}</div>
-          <div className="platform col-md-2 col-3">{platform}</div>
-          <Barrier remarks={remarks} />
-          <Bike remarks={remarks} />
-          <Warning remarks={remarks} />
-          <Status remarks={remarks} />
-        </div>
-        <div className="row row-add" key={`add:${this.props.dep.tripId}`}>
-          <Warntext remarks={remarks} />
-          <Stattext remarks={remarks} />
-        </div>
-      </React.Fragment>
-    );
+  let realtime = getTime(props.dep.when);
+  let plantime;
+  if (props.dep.when != null && props.dep.delay != null) {
+    plantime = getPlanTime(realtime, delayMin);
+  } else if (props.dep.when != null && delayMin === "?") {
+    plantime = realtime;
+    realtime = "";
+  } else {
+    plantime =
+      getTime(props.dep.formerScheduledWhen) ||
+      getTime(props.dep.scheduledWhen);
   }
-}
+  const product = props.dep.line.product;
+  const line = props.dep.line.name;
+  const { direction, remarks, platform } = props.dep;
+  return (
+    <React.Fragment>
+      <div className="row" key={props.dep.tripId}>
+        <Time time={plantime} />
+        <Time time={realtime} />
+        <div className="delay col-md-2 col-8">{delayMin}</div>
+        <Product product={product} line={line} />
+        <div className="line col-md-3 col-6">{line}</div>
+        <div className="direction col-md-8 col-13">{direction}</div>
+        <div className="platform col-md-2 col-3">{platform}</div>
+        <Barrier remarks={remarks} />
+        <Bike remarks={remarks} />
+        <Warning remarks={remarks} />
+        <Status remarks={remarks} />
+      </div>
+      <div className="row row-add" key={`add:${props.dep.tripId}`}>
+        <Warntext remarks={remarks} />
+        <Stattext remarks={remarks} />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Departure;
