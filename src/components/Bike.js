d10923d5f37e9c4873dc252b70713b7c8b6af339
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-13 13:56:57 +0200

    refactor Bike as a functional component

--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Bike extends Component {
-  render() {
-    for (let i = 0; i < this.props.remarks.length; i++) {
-      if (this.props.remarks[i].code === "FB") {
-        return (
-          <div className="bike col-md-1 col-6">
-            <span className="fas fa-bicycle" title="bike transport" />
-          </div>
-        );
-      }
-      return <div className="bike col-md-1 col-6" />;
+const Bike = props => {
+  for (let i = 0; i < props.remarks.length; i++) {
+    if (props.remarks[i].code === "FB") {
+      return (
+        <div className="bike col-md-1 col-6">
+          <span className="fas fa-bicycle" title="bike transport" />
+        </div>
+      );
     }
     return <div className="bike col-md-1 col-6" />;
   }
-}
+  return <div className="bike col-md-1 col-6" />;
+};
+
+export default Bike;
