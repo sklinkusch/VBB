ae4d8dfe21d9b67b526b6c585d623139ef84af5f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-14 10:41:08 +0200

    refactor Status as a functional component

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Status extends Component {
-  render() {
-    const remarks = this.props.remarks;
-    for (let i = 0; i < remarks.length; i++) {
-      if (remarks[i].type === "status") {
-        return (
-          <div className="status col-md-1 col-6">
-            <span className="fas fa-times" title={remarks[i].text} />
-          </div>
-        );
-      }
+const Status = props => {
+  const { remarks } = props;
+  for (let i = 0; i < remarks.length; i++) {
+    const { type, text } = remarks[i];
+    if (type === "status") {
+      return (
+        <div className="status col-md-1 col-6">
+          <span className="fas fa-times" title={text} />
+        </div>
+      );
     }
-    return <div className="status col-md-1 col-6" />;
   }
-}
+  return <div className="status col-md-1 col-6" />;
+};
+
+export default Status;
