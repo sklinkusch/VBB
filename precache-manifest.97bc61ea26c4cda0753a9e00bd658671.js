477ec9637c605c8145f448d36d17f033642fe512
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-10-23 20:16:29 +0200

    Updates

---
+++ precache-manifest.97bc61ea26c4cda0753a9e00bd658671.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "27a4e6fdc8464141b7a957dba093826a",
+    "url": "/VBB/static/media/fa-regular-400.27a4e6fd.woff2"
+  },
+  {
+    "revision": "f200523c977d376aa986",
+    "url": "/VBB/static/css/main.8af616ab.chunk.css"
+  },
+  {
+    "revision": "d72f7f825235a93630bf",
+    "url": "/VBB/static/js/runtime~main.4ceb75fb.js"
+  },
+  {
+    "revision": "e49b3ad24f7c43e03b1719796d2a8d78",
+    "url": "/VBB/static/media/soroush-karimi-192112-unsplash.min.e49b3ad2.jpg"
+  },
+  {
+    "revision": "2de7bfb853bd28bb2af4",
+    "url": "/VBB/static/js/2.a59164f7.chunk.js"
+  },
+  {
+    "revision": "9bfc6cc804e775c85195571e8e2664d3",
+    "url": "/VBB/static/media/bus.9bfc6cc8.svg"
+  },
+  {
+    "revision": "4a5465b04bdff505ae6055cceb1f55f0",
+    "url": "/VBB/static/media/express.4a5465b0.svg"
+  },
+  {
+    "revision": "d4f80c1cfa1d4efbdf2faa1cdbae0314",
+    "url": "/VBB/static/media/regional.d4f80c1c.svg"
+  },
+  {
+    "revision": "2c41595f1b774455c9ea67b64d5bb0e8",
+    "url": "/VBB/static/media/suburban.2c41595f.svg"
+  },
+  {
+    "revision": "1880e72d9e4b59c5980357c42793b6f8",
+    "url": "/VBB/static/media/subway.1880e72d.svg"
+  },
+  {
+    "revision": "ea60d5684c34ecdc2645be97c7c646fc",
+    "url": "/VBB/static/media/metro-tram.ea60d568.svg"
+  },
+  {
+    "revision": "ba07b10fc66aa6151ee79572809cdc3d",
+    "url": "/VBB/static/media/tram.ba07b10f.svg"
+  },
+  {
+    "revision": "bc6385c8db933d1c1eb8fc069ed9e864",
+    "url": "/VBB/static/media/metro-bus.bc6385c8.svg"
+  },
+  {
+    "revision": "6c2f44368fab31b4dcad1c0b4781550a",
+    "url": "/VBB/static/media/express-bus.6c2f4436.svg"
+  },
+  {
+    "revision": "f3b46a0de1ce19ed91d2565d8afb15e2",
+    "url": "/VBB/static/media/special-bus.f3b46a0d.svg"
+  },
+  {
+    "revision": "f0c4db903ee25fcd3fc7487eeaa2a9da",
+    "url": "/VBB/static/media/night-bus.f0c4db90.svg"
+  },
+  {
+    "revision": "ccc6ab35b01701734507a0d752a191a7",
+    "url": "/VBB/static/media/ferry.ccc6ab35.svg"
+  },
+  {
+    "revision": "f200523c977d376aa986",
+    "url": "/VBB/static/js/main.59d0aae0.chunk.js"
+  },
+  {
+    "revision": "8a8d8cd6f4c8a8e556d75eb45b317f29",
+    "url": "/VBB/static/media/fa-regular-400.8a8d8cd6.woff"
+  },
+  {
+    "revision": "3cefd3390001dcb1eb4629f61bba9185",
+    "url": "/VBB/static/media/fa-regular-400.3cefd339.ttf"
+  },
+  {
+    "revision": "ee87e7fcb588d700111c24d5ada28775",
+    "url": "/VBB/static/media/fa-regular-400.ee87e7fc.eot"
+  },
+  {
+    "revision": "5795bc0a3b45e06502187ca104557ca3",
+    "url": "/VBB/static/media/fa-brands-400.5795bc0a.woff2"
+  },
+  {
+    "revision": "55071ae1abc18726e23174192ebc0b68",
+    "url": "/VBB/static/media/fa-solid-900.55071ae1.woff2"
+  },
+  {
+    "revision": "90cd422fde1f01392a34aaec011a5dda",
+    "url": "/VBB/static/media/fa-brands-400.90cd422f.woff"
+  },
+  {
+    "revision": "56df75b6af75497c7a9a2b0847981f40",
+    "url": "/VBB/static/media/fa-solid-900.56df75b6.woff"
+  },
+  {
+    "revision": "003231a1f009db089ab16093ce59e4d2",
+    "url": "/VBB/static/media/fa-brands-400.003231a1.eot"
+  },
+  {
+    "revision": "2ec1097c081aa61e17956be4e8008cba",
+    "url": "/VBB/static/media/fa-brands-400.2ec1097c.ttf"
+  },
+  {
+    "revision": "2788556a1a246f0d0167ce5b4189151e",
+    "url": "/VBB/static/media/fa-regular-400.2788556a.svg"
+  },
+  {
+    "revision": "2eaaf2a78a47f2d9ae8d1d836eed5d24",
+    "url": "/VBB/static/media/fa-solid-900.2eaaf2a7.ttf"
+  },
+  {
+    "revision": "06d5f03672635f0a67010721e8e9929d",
+    "url": "/VBB/static/media/fa-solid-900.06d5f036.eot"
+  },
+  {
+    "revision": "3237e6d6e151b80ed2686fedbbb280f4",
+    "url": "/VBB/static/media/fa-brands-400.3237e6d6.svg"
+  },
+  {
+    "revision": "8e39d5d50ead4840070075889f5daeaa",
+    "url": "/VBB/static/media/fa-solid-900.8e39d5d5.svg"
+  },
+  {
+    "revision": "2de7bfb853bd28bb2af4",
+    "url": "/VBB/static/css/2.d878746e.chunk.css"
+  },
+  {
+    "revision": "5094d5525df75754bf550030944349b6",
+    "url": "/VBB/index.html"
+  }
+];
