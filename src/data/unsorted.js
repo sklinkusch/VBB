7b3a0e78f960083f423684bea78f566c23e6d8e9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-09-04 16:56:09 +0200

    rename Lustgarten to U Museumslinsel

--- src/data/unsorted.js
+++ src/data/unsorted.js
@@ -1,6 +1,7 @@
 const stopObject = require("vbb-stations/full.json");
 stopObject["900000470117"].name = "Cottbus, Beuchstr.";
 stopObject["900000210600"].name = "Wustermark, Brunnenplatz";
+stopObject["900000100537"].name = "U Museumsinsel (Berlin)";
 // console.log(stopObject["900000470117"]);
 const stopsUnsorted = Object.keys(stopObject).map(key => {
   return { id: stopObject[key].id, name: stopObject[key].name };
