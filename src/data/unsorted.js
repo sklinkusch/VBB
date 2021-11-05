422b7dce32847d07ffd8eebdf911e8ca5f37b1e7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-05 20:59:13 +0100

    add renamed stops (31 Oct 2021)

--- src/data/unsorted.js
+++ src/data/unsorted.js
@@ -1,7 +1,24 @@
 const stopObject = require("vbb-stations/full.json");
-stopObject["900000470117"].name = "Cottbus, Beuchstr.";
-stopObject["900000210600"].name = "Wustermark, Brunnenplatz";
-stopObject["900000100537"].name = "U Museumsinsel (Berlin)";
+// const vals = Object.values(stopObject).map(item => ({id: item.id, name: item.name})).filter(item => item.name.includes("Waltersdorfer Str"))
+// console.log(vals)
+stopObject["900000161517"].name = "Berlin, Alfred-Kowalke-Str.";
+stopObject["900000150050"].name = "Berlin, Große-Leege-Str./Bahnhofstr.";
+stopObject["900000140713"].name = "Berlin, Betriebshof Weißensee";
+stopObject["900000162504"].name = "Berlin, Blockdammweg";
+stopObject["900000120013"].name = "Berlin, Holteistr.";
+stopObject["900000152006"].name = "Berlin, Prendener Str.";
+stopObject["900000160006"].name = "Berlin, Heizkraftwerk Klingenberg";
+stopObject["900000180513"].name = "Berlin, Janitzkystr.";
+stopObject["900000110029"].name = "Berlin, Karl-Lade-Str.";
+stopObject["900000180010"].name = "Berlin, Mittelheide";
+stopObject["900000181505"].name = "Berlin, Weiskopffstr.";
+stopObject["900000186505"].name = "Berlin, Königsseestr.";
+stopObject["900000160538"].name = "Berlin, Scheffelstr.";
+stopObject["900000150515"].name = "Berlin, Orankestrand";
+stopObject["900000160511"].name = "Berlin, Paul-Junius-Str.";
+stopObject["900000100506"].name = "Berlin, Hannoversche Str.";
+stopObject["900000181501"].name = "Berlin, Volkspark Wuhlheide";
+stopObject["900000196003"].name = "Berlin, Grottewitzstr.";
 // console.log(stopObject["900000470117"]);
 const stopsUnsorted = Object.keys(stopObject).map(key => {
   return { id: stopObject[key].id, name: stopObject[key].name };
