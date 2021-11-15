11974496c9c63da99765564f70efd42f0cad4e35
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-15 20:41:54 +0100

    add locales for Tablehead

--- src/components/Tablehead.js
+++ src/components/Tablehead.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "../styles/Tablehead.scss";
+import getLocale from "./getLocale";
 
 export default function Tablehead() {
   return (
     <div className="row thead">
-      <div className="plantime thead col-md-2">sched.</div>
-      <div className="realtime thead col-md-2">real</div>
-      <div className="delay thead col-md-2">del.</div>
+      <div className="plantime thead col-md-2">{getLocale("scheduled")}</div>
+      <div className="realtime thead col-md-2">{getLocale("real")}</div>
+      <div className="delay thead col-md-2">{getLocale("delay")}</div>
       <div className="mean thead col-md-1" />
-      <div className="line thead col-md-3">line</div>
-      <div className="direction thead col-md-8">direction</div>
-      <div className="platform thead col-md-2">track</div>
+      <div className="line thead col-md-3">{getLocale("line")}</div>
+      <div className="direction thead col-md-8">{getLocale("direction")}</div>
+      <div className="platform thead col-md-2">{getLocale("track")}</div>
       <div className="barrier thead col-md-1" />
       <div className="bike thead col-md-1" />
       <div className="warning thead col-md-1" />
