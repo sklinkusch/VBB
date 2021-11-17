dd1054a96cb411a5a9e299193928b35a07ef9bb4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-17 23:04:27 +0100

    modify Filter for arrivals

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,12 +2,12 @@ import React from "react"
 import getLocale from "./getLocale"
 
 export default function Filter(props) {
-  const { filterField, filterSelector, filterData } = props
+  const { filterField, filterSelector, filterData, mode = "dep" } = props
   return (
     <div>
       <input
         type="text"
-        placeholder={getLocale("filterDepartures")}
+        placeholder={mode === "dep" ? getLocale("filterDepartures") : getLocale("filterArrivals")}
         ref={filterField}
         onInput={(event) => props.filterData(event)}
       />
