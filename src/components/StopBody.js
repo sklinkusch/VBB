9185c14ffffd4b794fff68d59323beefa813c791
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-05-29 14:18:44 +0200

    change back to function component

--- src/components/StopBody.js
+++ src/components/StopBody.js
@@ -1,17 +1,12 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { getDuration } from "./helpers"
 import StopName from "./StopName"
 import Error from "./Error"
 import TableData from "./TableData"
 
-class StopBody extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      newData: null
-    }
-  }
-  sortData = (data) => {
+export default function StopBody({ data, error, stop }) {
+  const [newData, setNewData] = useState(null)
+  const sortData = (data) => {
     if (data !== null && data !== undefined && data.length > 0) {
       const tempArray = data.map((element, index) => {
         const stopName = element.stop.name.toLowerCase()
@@ -41,7 +36,7 @@ class StopBody extends Component {
     }
     return undefined
   }
-  splitArray = async (data) => {
+  const splitArray = async (data) => {
     if (data !== null && data !== undefined && data.length > 0) {
       const stopsRaw = await data.map(e => e.stop.name)
       const stopsContracted = await stopsRaw.filter((val, ind, arr) => arr.indexOf(val) === ind)
@@ -56,37 +51,30 @@ class StopBody extends Component {
         return arr
       },[])
       if(resultArray.length > 0) {
-        this.setState({ newData: await resultArray })
+        setNewData(await resultArray)
       }
     }
   }
-  async componentDidMount() {
-    const sortedData = await this.sortData(this.props.data)
-    await this.splitArray(await sortedData)
-  }
-  async componentDidUpdate() {
-    const sortedData = await this.sortData(this.props.data)
-    await this.splitArray(await sortedData)
-  }
-  render() {
-    const { stop, error } = this.props
-    const { newData } = this.state
-    const mode = "dep"
-    const text = `In the next ${getDuration(stop.type || 'BLN')} minutes, no departures are planned for the station or stop you have chosen`
-    return (
-      <Fragment>
-        {stop && <StopName stop={stop} element="h2" />}
-        {error ? (<Error />) : (
-          newData !== undefined && newData !== null && newData.length > 0 ? (newData.map((depset = [], index) => {
-            if (depset.length > 0) {
-              return <TableData stop={depset[0].stop} data={depset} key={index} mode={mode} />
-            }
-            return null
-          })) : (<div>{text}</div>)
-        )}
-      </Fragment>
-    )
-  }
+  useEffect(() => {
+    async function fetchData() {
+      const sortedData = await sortData(data)
+      await splitArray(await sortedData)
+    }
+    fetchData()
+  },[data])
+  const mode = "dep"
+  const text = `In the next ${getDuration(stop.type || 'BLN')} minutes, no departures are planned for the station or stop you have chosen`
+  return (
+    <Fragment>
+      {stop && <StopName stop={stop} element="h2" />}
+      {error ? (<Error />) : (
+        newData !== undefined && newData !== null && newData.length > 0 ? (newData.map((depset = [], index) => {
+          if (depset.length > 0) {
+            return <TableData stop={depset[0].stop} data={depset} key={index} mode={mode} />
+          }
+          return null
+        })) : (<div>{text}</div>)
+      )}
+    </Fragment>
+  )
 }
-
-export default StopBody
