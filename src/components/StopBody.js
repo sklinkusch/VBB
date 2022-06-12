079db61432880f7632b303125c2521626333e8d7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-06-12 13:47:46 +0200

    add stops for Spandau

--- src/components/StopBody.js
+++ src/components/StopBody.js
@@ -71,6 +71,109 @@ function getHbf(lineName, direction) {
   return "S+U Berlin Hauptbahnhof" 
 }
 
+function getSpandau(id, lineName, direction) {
+  switch(id) {
+    case "900000029302": 
+      switch(lineName) {
+        case "M32":
+          if (direction.includes("Heidebergplan")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Döberitzer Weg")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Havelpark")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Brunsbütteler Damm")) return ["S+U Rathaus Spandau [Bus Altstädter Ring", 5]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "M36":
+          if (direction.includes("Haselhorst")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 7]
+          if (direction.includes("Am Omnibushof")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "M37":
+          if (direction.includes("Hahneberg")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Spektefeld")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 6]
+          if (direction.includes("Waldkrankenhaus")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 6]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "M45": 
+          if (direction.includes("Westend")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Zoologischer Garten")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Johannesstift")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 8]
+          if (direction.includes("Klinkeplatz")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 8]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "X33":
+          if (direction.includes("Galenstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 3]
+          if (direction.includes("Wilhelmsruher Damm")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 9]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "X37":
+          if (direction.includes("Falkensee")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 6]
+          if (direction.includes("Ruhleben")) return ("S+U Rathaus Spandau [Bus Altstädter Ring]", 5)
+          if (direction.includes("Galenstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "134":
+        case "N34":
+          if (direction.includes("Hottengrund")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Alt-Kladow")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Alt-Gatow")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Gutsstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Gatower Str")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Galenstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Kisseln")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 8]
+          if (direction.includes("Wasserwerk")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 8]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "135":
+        case "638":
+          if (direction.includes("Alt-Kladow")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Hottengrund")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          if (direction.includes("Potsdam")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "136":
+          if (direction.includes("Hennigsdorf")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 7]
+          if (direction.includes("Tongaweg")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 7]
+          if (direction.includes("Aalemannufer")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 7]
+          if (direction.includes("Werderstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 7]
+          if (direction.includes("Galenstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 3]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "137":
+          if (direction.includes("Reimerweg")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 5]
+          if (direction.includes("Freudstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 6]
+          if (direction.includes("Westerwaldstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 6]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+        case "337":
+          if (direction.includes("Galenstr")) return ["S+u Rathaus Spandau [Bus Altstädter Ring]", 3]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 6]
+        case "671":
+        case "N7":
+          if (direction.includes("Galenstr")) return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 3]
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 9]
+        case "N30":
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", 4]
+        default:
+          return ["S+U Rathaus Spandau [Bus Altstädter Ring]", null]
+      }
+    case "900000029371":
+      switch(lineName){
+        case "X33":
+        case "136":
+        case "337":
+        case "671":
+        case "N7":
+          return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 1]
+        case "N30":
+          return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 2]
+        case "130":
+          if(direction.includes("Ruhleben")) return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 1]
+          if(direction.includes("Stresow")) return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 1]
+          if(direction.includes("Zeppelinstr")) return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 2]
+          if(direction.includes("Westerwaldstr")) return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 2]
+          if(direction.includes("Waldkrankenhaus")) return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 2]
+          return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 1]
+        case "237":
+          if(direction.includes("Isenburger")) return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 2]
+          return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 1]
+        default:
+          return ["S+U Rathaus Spandau [Bus Seegef. Str.]", 1]
+      }
+    default:
+      return ["S+U Rathaus Spandau", null]
+  }
+}
+
 export default function StopBody({ data, error, stop }) {
   const [newData, setNewData] = useState(null)
   const sortData = (data) => {
@@ -139,6 +242,11 @@ export default function StopBody({ data, error, stop }) {
           const newStopName = getHbf(lineName, direction)
           const newStop = { ...stop, name: newStopName }
           return { ...e, stop: newStop }
+        }
+        if(["900000029302", "900000029371"].includes(id) && product === "bus") {
+          const [newStopName, trackNo] = getSpandau(id, lineName, direction)
+          const newStop = { ...stop, name: newStopName }
+          return { ...e, stop: newStop, platform: trackNo }
         }
         return e
       })
