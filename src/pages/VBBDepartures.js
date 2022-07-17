d8658e216574be3b84b3ef038b39615011e76d40
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-07-17 09:54:17 +0200

    change to HashRouter

--- src/pages/VBBDepartures.js
+++ src/pages/VBBDepartures.js
@@ -1,15 +1,26 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import LegendDep from "../components/LegendDep"
 import Timetable from "../components/Timetable"
 // const LegendDep = lazy(() => import("../components/LegendDep"))
 // const Timetable = lazy(() => import("../components/Timetable"))
 
 function App() {
+  let pathProps
+  switch(process.env.NODE_ENV) {
+    case "development":
+       pathProps = {}
+      break
+    case "production":
+      pathProps = { basename: process.env.PUBLIC_URL }
+      break
+    default:
+      pathProps = { basename: process.env.PUBLIC_URL }
+  }
   return (
     <div>
       <LegendDep />
-      <Router basename={`${process.env.PUBLIC_URL}`}>
+      <Router { ...pathProps }>
         <Routes>
           <Route path="/:stopId" element={<Timetable />} />
           <Route path="/" element={< Timetable />} />
