d2319311d7a076cc7e516e4557231b4d88d21a7c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-08-29 21:22:38 +0200

    fix import

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from 'src/components/App.js'
+import App from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
