157b21fdfcb600e5c37440674e32f89748f50e64
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-15 22:45:02 +0200

    add routing in main component

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,38 @@
 /** @jsxImportSource theme-ui */
-// import { Suspense, lazy } from "react"
-// import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom"
+import { Suspense, lazy } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
-import VBBDepartures from "../pages/VBBDepartures"
-// const VBBDepartures = lazy(() => import("../pages/VBBDepartures"))
-// const VBBArrivals = lazy(() => import("../pages/VBBArrivals"))
+// import VBBDepartures from "../pages/VBBDepartures"
+const VBBDepartures = lazy(() => import("../pages/VBBDepartures"))
+const VBBArrivals = lazy(() => import("../pages/VBBArrivals"))
 
 function App() {
-  return (
-    <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
-      <Header />
-      <VBBDepartures />
-      <Footer />
-    </div>
-  )
   // return (
-  //   <Router basename={`/${process.env.PUBLIC_URL}`}>
-  //     <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
-  //       <Header />
-  //         <Suspense fallback={<div>Loading...</div>}>
-  //           <Switch>
-  //             <Route path="/arrivals">
-  //               <VBBArrivals />
-  //             </Route>
-  //             <Route path="/departures">
-  //               <VBBDepartures />
-  //             </Route>
-  //             <Route path="/" exact>
-  //               <Redirect to="/departures" />
-  //             </Route>
-  //             <Route path="*">
-  //               <Redirect to="/departures" />
-  //             </Route>
-  //           </Switch>
-  //         </Suspense>
-  //       <Footer />
-  //     </div>
-  //   </Router>
+  //   <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
+  //     <Header />
+  //     <VBBDepartures />
+  //     <Footer />
+  //   </div>
   // )
+  return (
+    <Router basename={`/${process.env.PUBLIC_URL}`}>
+      <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
+        <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/arrivals/:id" element={<VBBArrivals />} />
+              <Route path="/arrivals" element={<VBBArrivals />} />
+              <Route path="/departures/:id" element={<VBBDepartures />} />
+              <Route path="/departures" element={<VBBDepartures />} />
+              <Route path="" exact={true} element={<VBBDepartures />} />
+              <Route path="*" element={<VBBDepartures />} />
+            </Routes>
+          </Suspense>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App
