06cc03a4e5fd3a55d4291e305b149b5c2d61fd3b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-15 22:45:42 +0200

    add dynamic linking in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource theme-ui */
 import { useDebugState } from "use-named-state"
-import { Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
-// import { NavLink as RRNavLink, withRouter } from "react-router-dom"
+import { Navbar, NavbarBrand, NavbarToggler, NavLink, Collapse, Nav, NavItem } from "reactstrap";
+import { NavLink as RRNavLink, useLocation } from "react-router-dom"
 import getLocale from "./getLocale";
 
 const Header = () => {
@@ -9,21 +9,24 @@ const Header = () => {
   const toggle = () => {
     setOpen(!isOpen)
   }
+  const location = useLocation()
+  const { pathname = "" } = location
+  const id = pathname.substring(1).split("/")[1] || null
   return (
     <header sx={{ backgroundColor: "darkblue", width: "85%", mx: "auto", my: "20px", padding: "10px", borderRadius: "15px", 'nav': { backgroundColor: "darkblue !important" }, '.navbar-brand': { color: "white"}}}>
       <Navbar color="dark" dark expand="lg">
         <NavbarBrand href="/">{getLocale("VBBInfo")}</NavbarBrand>
         <NavbarToggler onClick={toggle} />
-        {/* <Collapse isOpen={isOpen} navbar>
+        <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink to="/" exact tag={RRNavLink} className={({isActive}) => isActive ? "active" : null}>{getLocale("Depart")}</NavLink>
+              <NavLink to={id ? `/departures/${id}` : '/departures'} exact tag={RRNavLink} className={({isActive}) => isActive ? "active" : null}>{getLocale("Depart")}</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink to="/arrivals" tag={RRNavLink} className={({isActive}) => isActive ? "active" : null}>{getLocale("Arrive")}</NavLink>
+              <NavLink to={id ? `/arrivals/${id}` : '/arrivals'} tag={RRNavLink} className={({isActive}) => isActive ? "active" : null}>{getLocale("Arrive")}</NavLink>
             </NavItem>
           </Nav>
-        </Collapse> */}
+        </Collapse>
       </Navbar>
     </header>
   );
