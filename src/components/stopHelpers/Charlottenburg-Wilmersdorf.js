6f8ab8b897c88951b1c7cb43fb85a58e4c45308e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-18 16:37:52 +0200

    update stops for Westend

---
+++ src/components/stopHelpers/Charlottenburg-Wilmersdorf.js
@@ -0,0 +1,269 @@
+export function getCharlottenburg(id, mode, lineName, direction, provenance) {
+  if (mode === "arr") {
+    switch (id) {
+      case "900000024101":
+        return ["S Charlottenburg [Bus Stuttgarter Platz]", 5]
+      case "900000024103":
+        if(provenance.includes("Hertzallee")) return ["S Charlottenburg/Gervinusstr.", 7]
+        if(provenance.includes("Blissestr")) return ["S Charlottenburg/Gervinusstr.", 7]
+        return ["S Charlottenburg/Gervinusstr.", 6]
+      case "900000024202":
+        if(provenance.includes("Hertzallee")) return ["U Wilmersdorfer Str. [Bus Kantstr.]", 2]
+        if(provenance.includes("Savignyplatz")) return ["U Wilmersdorfer Str. [Bus Kantstr.]", 2]
+        if(["X49", "309"].includes(lineName)) return ["U Wilmersdorfer Str. [Bus Kantstr.]", 2]
+        return ["U Wilmersdorfer Str. [Bus Kantstr.]", 1]
+      case "900000024205":
+        if(["309", "310"].includes(lineName)) return ["U Wilmersdorfer Str./S Charlottenburg", 4]
+        return ["U Wilmersdorfer Str./S Charlottenburg", 3]
+      default: return ["S Charlottenburg [Bus]", null]
+    }
+  } else {
+    switch (id) {
+      case "900000024101":
+        return ["S Charlottenburg [Bus Stuttgarter Platz]", 5]
+      case "900000024103":
+        if(direction.includes("Zoo")) return ["S Charlottenburg/Gervinusstr.", 6]
+        return ["S Charlottenburg/Gervinusstr.", 7]
+      case "900000024202":
+        if(direction.includes("Zoo")) return ["U Wilmersdorfer Str. [Bus Kantstr.]", 1]
+        if(direction.includes("Savignyplatz")) return ["U Wilmersdorfer Str. [Bus Kantstr.]", 1]
+        return ["U Wilmersdorfer Str. [Bus Kantstr.]", 2]
+      case "900000024205":
+        if(lineName === "X49") return ["U Wilmersdorfer Str./S Charlottenburg", 3]
+        return ["U Wilmersdorfer Str./S Charlottenburg", 4]
+      default: return ["S Charlottenburg [Bus]", null]
+    }
+  }
+}
+
+export function getGrunewald(lineName, direction) {
+  switch(lineName) {
+    case "M19":
+    case "186":
+      return "S Grunewald [Karmielplatz]"
+    case "349":
+      return "S Grunewald [Schmetterlingsplatz]"
+    default: 
+      return "S Grunewald"
+  }
+}
+
+export function getJungfernheide(id, mode, lineName, direction, provenance) {
+  if (mode === "arr") {
+    switch(id) {
+      case "900000020201":
+        switch(lineName) {
+          case "M21":
+          case "X21":
+            if(provenance.includes("Goerdelersteg")) return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 3]
+            return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 5]
+          case "M27":
+          case "N7":
+            if (provenance.includes("Galenstr")) return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 5]
+            return ["S+U Jungfernheide [Bus Olbersstr.]", 6]
+          default:
+            return ["S+U Jungfernheide", null]
+        }
+      case "900000020207":
+        switch(lineName) {
+          case "M27":
+          case "N7":
+            return ["Tegeler Weg/S Jungfernheide", 2]
+          case "109":
+            if (provenance.includes("Hertzallee")) return ["Tegeler Weg/S Jungfernheide", 2]
+            return ["Tegeler Weg/S Jungfernheide", 1]
+          default:
+            return ["Tegeler Weg/S Jungfernheide", 2]
+        }
+      default: return ["S+U Jungfernheide", null]
+    }
+  } else {
+    switch(id) {
+      case "900000020201":
+        switch(lineName) {
+          case "M21":
+          case "X21":
+            if(direction.includes("Goerdelersteg")) return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 5]
+            return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 3]
+          case "M27":
+          case "N7":
+            if(direction.includes("Flughafen")) return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 5]
+            if(direction.includes("Rudow")) return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 5]
+            if(direction.includes("Pankow")) return ["S+U Jungfernheide [Bus Max-Dohrn-Str.]", 5]
+            return ["S+U Jungfernheide [Bus Olbersstr.]", 6]
+          default:
+            return ["S+U Jungfernheide", null]
+        }
+      case "900000020207":
+        switch(lineName) {
+          case "M27":
+          case "N7":
+            return ["Tegeler Weg/S Jungfernheide", 2]
+          case "109":
+            if (direction.includes("Zoologischer Garten")) return ["Tegeler Weg/S Jungfernheide", 1]
+            return ["Tegeler Weg/S Jungfernheide", 2]
+          default:
+            return ["Tegeler Weg/S Jungfernheide", 2]
+        }
+      default: return ["S+U Jungfernheide", null]
+    }
+  }
+}
+
+export function getRuhleben(mode, lineName, direction, provenance) {
+  if (mode === 'arr') {
+    switch (lineName) {
+      case "M45":
+        if (provenance.includes("Hertzallee")) return "U Ruhleben [Bus Hempelsteig]"
+        if (provenance.includes("Westend")) return "U Ruhleben [Bus Hempelsteig]"
+        return "U Ruhleben [Bus Charlottenburger Chaussee]"
+      case "130":
+      case "N2":
+        return "U Ruhleben [Bus Hempelsteig]"
+      case "131":
+      case "X37":
+        return "U Ruhleben [Bus]"
+      default: return "U Ruhleben [Bus]"
+    }
+  } else {
+    switch (lineName) {
+      case "M45":
+        if (direction.includes("Zoologischer Garten")) return "U Ruhleben [Bus Charlottenburger Chaussee]"
+        if (direction.includes("Westend")) return "U Ruhleben [Bus Charlottenburger Chaussee]"
+        return "U Ruhleben [Bus Hempelsteig]"
+      case "131":
+      case "X37":
+        if (direction.includes("Ruhleben")) return "U Ruhleben [Bus Charlottenburger Chaussee]"
+        return "U Ruhleben [Bus Hempelsteig]"
+      case "130":
+      case "N2":
+        return "U Ruhleben [Bus]"
+      default: return "U Ruhleben [Bus]"
+    }
+  }
+}
+
+export function getWestend(id) {
+    switch(id) {
+      case "900000026207": return "S Westend [Bus]"
+      case "900000020204": return "Sophie-Charlotten-Str."
+      default: return "S Westend [Bus]"
+    }
+}
+
+export function getZooBusStops(id, mode, lineName, direction, provenance) {
+  if (mode === 'arr') {
+    switch(id) {
+      case "900000023201":
+        if(!provenance.includes("Hertzallee")) {
+          return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 10]
+        }
+        switch(lineName) {
+          case "X10":
+          case "109":
+          case "110":
+          case "N7X":
+          case "N10":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 4]
+          case "M49":
+          case "X34":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 5]
+          case "N9":
+            if(provenance.includes("Osloer")) return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 5]
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 5]
+          case "M45":
+          case "245":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 6]
+          case "100":
+          case "200":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+          case "N2":
+            if(provenance.includes("Ruhleben")) return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+          case "M46":
+          case "204":
+          case "N1":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 8]
+          case "249":
+          case "N26":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 9]
+          default:
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", null]
+        }
+      case "900000023172":
+        switch (lineName) {
+          case "M45":
+          case "245":
+            if (!provenance.includes("S+U Zoologischer Garten") && !provenance.includes("Hertzallee")) {
+              return ["S+U Zoologischer Garten/Jebensstr.", 1]
+            }
+            return ["S+U Zoologischer Garten/Jebensstr.", 2]
+          case "100":
+          case "200":
+          case "N2":
+          case "N9":
+            return ["S+U Zoologischer Garten/Jebensstr.", 3]
+          default:
+            return ["S+U Zoologischer Garten/Jebensstr.", null]
+        }
+      default: return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", null]
+    }
+  } else {
+    switch(id) {
+      case "900000023201":
+        if(direction.includes("Hertzallee") || direction.includes("Fahrt endet hier")) {
+          return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 10]
+        }
+        switch(lineName) {
+          case "X10":
+          case "109":
+          case "110":
+          case "N7X":
+          case "N10":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 4]
+          case "M49":
+          case "X34":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 5]
+          case "N9":
+            if(direction.includes("Steglitz")) return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 5]
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 5]
+          case "M45":
+          case "245":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 6]
+          case "100":
+          case "200":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+          case "N2":
+            if(direction.includes("Pankow")) return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+            if(direction.includes("Alexanderplatz")) return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 7]
+          case "M46":
+          case "204":
+          case "N1":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 8]
+          case "249":
+          case "N26":
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", 9]
+          default:
+            return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", null]
+        }
+      case "900000023172":
+        switch(lineName) {
+          case "M45":
+          case "245":
+            if (direction.includes("S+U Zoologischer Garten") || direction.includes("Hertzallee")) {
+              return ["S+U Zoologischer Garten/Jebensstr.", 1]
+            }
+            return ["S+U Zoologischer Garten/Jebensstr.", 2]
+          case "100":
+          case "200":
+          case "N2":
+          case "N9":
+            return ["S+U Zoologischer Garten/Jebensstr.", 3]
+          default:
+            return ["S+U Zoologischer Garten/Jebensstr.", null]
+        }
+      default: return ["S+U Zoologischer Garten [Bus Hardenbergplatz]", null]
+    }
+  }
+}
