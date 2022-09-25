6fdecf96a3868694ccbc316f37927625b29a903e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-25 22:52:37 +0200

    change Filter to typescript

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,15 @@
 /** @jsxImportSource theme-ui */
+import React, { ChangeEventHandler } from "react"
 import getLocale from "./getLocale"
 
-export default function Filter(props) {
+type Props = {
+  mode: string,
+  filterField: React.Ref<HTMLInputElement>,
+  filterSelector: React.Ref<HTMLSelectElement>
+  filterData: ChangeEventHandler
+}
+
+export default function Filter(props: Props) {
   const { filterField, filterSelector, filterData, mode = "dep" } = props
   return (
     <div>
@@ -9,7 +17,7 @@ export default function Filter(props) {
         type="text"
         placeholder={mode === "dep" ? getLocale("filterDepartures") : getLocale("filterArrivals")}
         ref={filterField}
-        onInput={(event) => props.filterData(event)}
+        onChange={(event) => props.filterData(event)}
         sx={{ padding: "5px", opacity: 1, backgroundColor: "#fff", width: "300px", maxWidth: "90%" }}
       />
       <select ref={filterSelector} onChange={filterData} sx={{ padding: "5px", backgroundColor: "#ccc", opacity: 1, width: "400px", maxWidth: "90%", mx: "10px" }}>
