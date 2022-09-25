9ea9c7d7675a6e6f45e9643556443ef145144240
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-25 15:21:17 +0200

    change Error to typescript

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource theme-ui */
 
-export default function Error(props) {
+type Props = any
+
+export default function Error(props: Props) {
   return (
     <p className="error" sx={{ color: "red", fontWeight: "bold" }}>Your request could not be handled by the server.</p>
   );
