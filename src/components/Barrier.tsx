5f9c7c2f230ccbdd485c420ea706ce63cd6c0fa8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-25 11:12:56 +0200

    change Barrier to typescript

--- src/components/Barrier.tsx
+++ src/components/Barrier.tsx
@@ -1,17 +1,22 @@
 /** @jsxImportSource theme-ui */
 import { Fragment } from "react"
 
-const Barrier = ({ remarks }) => {
+type Props = {
+  remarks: {
+    code: string
+  }[]
+}
+
+const Barrier = ({ remarks }: Props) => {
   const barrierRemarks = remarks.filter((remark) => remark.code === "bf")
-  const styles = { textAlign: "center", gridColumn: ["1 / span 6", "21 / span 1"]}
   return (
     <Fragment>
       {barrierRemarks.length > 0 ? (
-        <div className="barrier" sx={{ ...styles }}>
+        <div className="barrier" sx={{ textAlign: "center", gridColumn: ["1 / span 6", "21 / span 1"]}}>
           <span className="fas fa-wheelchair" title="barrier-free" />
         </div>
       ) : (
-        <div className="barrier" sx={{ ...styles }} />
+        <div className="barrier" sx={{ textAlign: "center", gridColumn: ["1 / span 6", "21 / span 1"]}} />
       )}
     </Fragment>
   )
