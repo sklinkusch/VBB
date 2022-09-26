3c168eef5c97866ec4550704a57f0cb01b2927a2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-26 20:25:57 +0200

    change Input to typescript

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,18 @@
 /** @jsxImportSource theme-ui */
-import { Fragment } from "react"
+import { ChangeEventHandler, Fragment } from "react"
 import getLocale from "./getLocale"
 
-export default function Input({ filterStops, inputField }) {
+type Props = {
+  filterStops: ChangeEventHandler,
+  inputField: React.Ref<HTMLInputElement>
+}
+
+export default function Input({ filterStops, inputField }: Props) {
   return (
     <Fragment>
       <input
         type="text"
-        onInput={(event) => filterStops(event)}
+        onChange={(event) => filterStops(event)}
         ref={inputField}
         placeholder={getLocale("filterStops")}
         sx={{ padding: "5px", opacity: 1, backgroundColor: "#fff", width: "300px", maxWidth: "90%" }}
