ccdad7882523964cf9d3ea1da852c8ac4979ed12
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-26 21:34:15 +0200

    change P to typescript

--- src/data/P.ts
+++ src/data/P.ts
@@ -1,23 +1,28 @@
 import { remainingStops as stopsUnsorted } from "./OSL";
 
+type Stop = {
+  id: string,
+  name: string
+}
+
 const stations = stopsUnsorted.filter(
-  stop =>
+  (stop: Stop) =>
     stop.name.includes("S Potsdam Hauptbahnhof") ||
     stop.name.includes("S Babelsberg") ||
     stop.name.includes("S Griebnitzsee")
 );
 const remainingAfterStations = stopsUnsorted.filter(
-  stop => stations.indexOf(stop) === -1
+  (stop: Stop) => stations.indexOf(stop) === -1
 );
-const stopsRaw = remainingAfterStations.filter(stop =>
+const stopsRaw = remainingAfterStations.filter((stop: Stop) =>
   stop.name.startsWith("Potsdam,")
 );
 const remainingStops = remainingAfterStations.filter(
-  stop => stopsRaw.indexOf(stop) === -1
+  (stop: Stop) => stopsRaw.indexOf(stop) === -1
 );
 // console.log(`after P: ${remainingStops.length}`);
 
-const stops = stopsRaw.map(stop => {
+const stops = stopsRaw.map((stop: Stop) => {
   return {
     id: stop.id,
     name: stop.name,
