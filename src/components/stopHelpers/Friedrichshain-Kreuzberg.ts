2bc0acc533ba66a7cf88055cd4710fd05327204e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-27 19:53:50 +0200

    change Friedrichshain-Kreuzberg to typescript

--- src/components/stopHelpers/Friedrichshain-Kreuzberg.ts
+++ src/components/stopHelpers/Friedrichshain-Kreuzberg.ts
@@ -1,4 +1,6 @@
-export function getAnhalterBahnhof(lineName) {
+type Dir = string | null
+
+export function getAnhalterBahnhof(lineName: string) {
   switch (lineName) {
     case "M29": return "S Anhalter Bahnhof [Bus Schöneberger Str.]"
     case "M41": return "S Anhalter Bahnhof [Bus Stresemannstr.]"
@@ -6,8 +8,8 @@ export function getAnhalterBahnhof(lineName) {
   }
 }
 
-export function getWarschauerStr(mode, lineName, direction, provenance) {
-  if (mode === "arr") {
+export function getWarschauerStr(mode: string, lineName: string, direction: Dir, provenance: Dir) {
+  if (mode === "arr" && provenance !== null) {
     switch (lineName) {
       case "M10": return ["S+U Warschauer Str. [Warschauer Str.]", 4]
       case "300": return ["S+U Warschauer Str. [Tamara-Danz-Str.]", 8]
@@ -16,7 +18,7 @@ export function getWarschauerStr(mode, lineName, direction, provenance) {
         return ["S+U Warschauer Str. [Warschauer Str.]", 3]
       default: return ["S+U Warschauer Str.", null]
     }
-  } else {
+  } else if (mode === 'dep' && direction !== null) {
     switch (lineName) {
       case "M10": return ["S+U Warschauer Str. [Warschauer Str.]", 5]
       case "300": return ["S+U Warschauer Str. [Tamara-Danz-Str.]", 7]
