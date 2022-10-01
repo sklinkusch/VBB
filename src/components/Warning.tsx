fb5168b5720675a4bc2b78d4976e629777ba8e4f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-01 10:46:53 +0200

    change type of Warning

--- src/components/Warning.tsx
+++ src/components/Warning.tsx
@@ -1,25 +1,6 @@
 /** @jsxImportSource theme-ui */
 
 type Remarks = {
-  categories: number[] | undefined,
-  code: string | undefined,
-  company: string | undefined,
-  icon: {
-    title: string | null | undefined,
-    type: string | undefined
-  },
-  id: string | undefined,
-  modified: string | undefined,
-  priority: number | undefined | null,
-  products: {
-    bus: boolean | undefined,
-    express: boolean | undefined,
-    ferry: boolean | undefined,
-    regional: boolean | undefined,
-    suburban: boolean | undefined,
-    subway: boolean | undefined,
-    tram: boolean | undefined
-  },
   summary: string | null | undefined,
   text: string,
   type: string,
