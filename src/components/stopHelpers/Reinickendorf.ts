80bd8d1999489685ad65423ee5e612ecca5bcdb8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-02 14:00:06 +0200

    add Waidmannslust to stops

--- src/components/stopHelpers/Reinickendorf.ts
+++ src/components/stopHelpers/Reinickendorf.ts
@@ -1,7 +1,16 @@
-export function getWilhelmsruh() {
-  return "S Wilhelmsruh [Bus Kopenhagener Str.]"
-}
-
 export function getSchönholz() {
   return "S Schönholz [Bus Provinzstr.]"
+}
+
+export function getWaidmannslust(lineName: string) {
+  switch (lineName) {
+    case "222": return "S Waidmannslust [Bus Waidmannsluster Damm]"
+    case "322": return "S Waidmannslust [Bus J.-Jaurès-Str.]"
+    case "N22": return "S Waidmannslust [Bus Waidmannsluster Damm]"
+    default: return "S Waidmannslust [Bus Waidmannsluster Damm]"
+  }
+}
+
+export function getWilhelmsruh() {
+  return "S Wilhelmsruh [Bus Kopenhagener Str.]"
 }
--- src/components/stopHelpers/index.ts
+++ src/components/stopHelpers/index.ts
@@ -4,7 +4,7 @@ import { getLichtenberg } from "./Lichtenberg"
 import { getFriedrichsfeldeOst, getMahlsdorf, getMarzahn } from "./Marzahn-Hellersdorf"
 import { getAlex, getBrandenburgerTor, getFriedrichstr, getGesundbrunnen, getHbf, getHumboldthain, getNordbahnhof, getOranienburgerStr, getPotsdamerPlatz } from "./Mitte"
 import { getBornholmerStr, getPankow, getWollankstr } from "./Pankow"
-import { getSchönholz, getWilhelmsruh } from "./Reinickendorf"
+import { getSchönholz, getWaidmannslust, getWilhelmsruh } from "./Reinickendorf"
 import { getSpandau } from "./Spandau"
 import { getBotanischerGarten, getFeuerbachstr, getLichterfeldeWest, getMexikoplatz, getNikolassee, getSteglitz, getSundgauerStr, getWannsee, getZehlendorf } from "./Steglitz-Zehlendorf"
 import { getJuliusLeberBrücke, getSchöneberg, getSüdkreuz } from "./Tempelhof-Schöneberg"
@@ -332,6 +332,10 @@ export function changeStopObject (mode: string, oldStopObject: Data) {
         return { ...oldStopObject, stop: newStop }
       case "900000023301":
         newStopName = getUhlandstr()
+        newStop = { ...stop, name: newStopName }
+        return { ...oldStopObject, stop: newStop }
+      case "900000094101":
+        newStopName = getWaidmannslust(lineName)
         newStop = { ...stop, name: newStopName }
         return { ...oldStopObject, stop: newStop }
       case "900000053301":
