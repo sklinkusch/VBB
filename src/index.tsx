eaf1a2078c855f929c8dc2d8d318c2f1f92aa002
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-02 10:32:16 +0200

    change index to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { createRoot } from "react-dom/client"
+import ReactDOM from "react-dom/client"
 import "./styles/index.css"
 import App from "./components/App"
 import reportWebVitals from "./reportWebVitals"
@@ -7,9 +7,14 @@ import "@fortawesome/fontawesome-free/js/all.min.js"
 import { ThemeProvider } from "theme-ui"
 import theme from "./styles/theme"
 
-const container = document.getElementById("root")
-const root = createRoot(container)
-root.render(<ThemeProvider theme={theme}><App /></ThemeProvider>)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
