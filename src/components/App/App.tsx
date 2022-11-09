2c03ba48bb583960afa23d2c8c80318986aa19c3
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-11-09 20:52:43 +0100

    increase opacity of background

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,31 +8,41 @@ const VBBDepartures = lazy(() => import("../../pages/VBBDepartures"))
 const VBBArrivals = lazy(() => import("../../pages/VBBArrivals"))
 
 function App() {
-  // return (
-  //   <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
-  //     <Header />
-  //     <VBBDepartures />
-  //     <Footer />
-  //   </div>
-  // )
-  return (
-    <Router basename={`/${process.env.PUBLIC_URL}`}>
-      <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
-        <Header />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/arrivals/:id" element={<VBBArrivals />} />
-              <Route path="/arrivals" element={<VBBArrivals />} />
-              <Route path="/departures/:id" element={<VBBDepartures />} />
-              <Route path="/departures" element={<VBBDepartures />} />
-              <Route path="" element={<VBBDepartures />} />
-              <Route path="*" element={<VBBDepartures />} />
-            </Routes>
-          </Suspense>
-        <Footer />
-      </div>
-    </Router>
-  )
+	// return (
+	//   <div className="App container" sx={{ textAlign: "center", backgroundColor: "whitesmoke", opacity: 0.8, width: "100%", minHeight: "96vh", fontFamily: "body" }}>
+	//     <Header />
+	//     <VBBDepartures />
+	//     <Footer />
+	//   </div>
+	// )
+	return (
+		<Router basename={`/${process.env.PUBLIC_URL}`}>
+			<div
+				className="App container"
+				sx={{
+					textAlign: "center",
+					backgroundColor: "whitesmoke",
+					opacity: 0.95,
+					width: "100%",
+					minHeight: "96vh",
+					fontFamily: "body",
+				}}
+			>
+				<Header />
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path="/arrivals/:id" element={<VBBArrivals />} />
+						<Route path="/arrivals" element={<VBBArrivals />} />
+						<Route path="/departures/:id" element={<VBBDepartures />} />
+						<Route path="/departures" element={<VBBDepartures />} />
+						<Route path="" element={<VBBDepartures />} />
+						<Route path="*" element={<VBBDepartures />} />
+					</Routes>
+				</Suspense>
+				<Footer />
+			</div>
+		</Router>
+	)
 }
 
 export default App
