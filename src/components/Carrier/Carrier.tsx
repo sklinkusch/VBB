29325130b14afbf864c9fad9d88e74b5daac1f2b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-05-15 20:34:59 +0200

    add icon for UVG

--- src/components/Carrier/Carrier.tsx
+++ src/components/Carrier/Carrier.tsx
@@ -28,6 +28,7 @@ import SRS from "../../images/carriers/srs.svg"
 import STE from "../../images/carriers/ste.svg"
 import SVF from "../../images/carriers/svf.svg"
 import Sabinchen from "../../images/carriers/sabinchen.png"
+import UVG from "../../images/carriers/uvg.svg"
 import VerkehrsbetriebPotsdam from "../../images/carriers/VerkehrsbetriebPotsdam.jpg"
 import VTF from "../../images/carriers/vtf.svg"
 
@@ -217,6 +218,12 @@ const Carrier = ({operator}: Props) => {
       return (
         <div sx={{ gridColumn: ["21 / span 4", "21 / span 4","25 / span 2"] }} title={operator.name}>
           <img src={SVF} alt={operator.name} sx={ styles } /> 
+        </div>
+      )
+    case "uckermarkische-verkehrsgesellschaft-mbh":
+      return (
+        <div sx={{ gridColumn: ["21 / span 4", "21 / span 4","25 / span 2"] }} title={operator.name}>
+          <img src={UVG} alt={operator.name} sx={ styles } /> 
         </div>
       )
     case "verkehrsbetrieb-potsdam-gmbh":
