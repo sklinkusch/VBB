ab4a4a66503fb6d604748cce904d7866cc8465d0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-05-15 19:16:09 +0200

    add icon for Busverkehr Gerd Schmidt

--- src/components/Carrier/Carrier.tsx
+++ src/components/Carrier/Carrier.tsx
@@ -7,6 +7,7 @@ import BOS from "../../images/carriers/bos.svg"
 import BVG from '../../images/carriers/bvg.svg'
 import DB from "../../images/carriers/db.svg"
 import FlixTrain from "../../images/carriers/flixtrain.svg"
+import GSchmidt from "../../images/carriers/gerd-schmidt.png"
 import Havelbus from "../../images/carriers/havelbus.svg"
 import MRB from "../../images/carriers/mrb.svg"
 import NEB from "../../images/carriers/neb.svg"
@@ -68,6 +69,12 @@ const Carrier = ({operator}: Props) => {
       return (
         <div sx={{ gridColumn: ["21 / span 4", "21 / span 4","25 / span 2"] }} title={operator.name}>
           <img src={BVG} alt={operator.name} sx={ styles } /> 
+        </div>
+      )
+    case "busverkehr-gerd-schmidt":
+      return (
+        <div sx={{ gridColumn: ["21 / span 4", "21 / span 4","25 / span 2"] }} title={operator.name}>
+          <img src={GSchmidt} alt={operator.name} sx={ styles } /> 
         </div>
       )
     case "db-regio-ag":
