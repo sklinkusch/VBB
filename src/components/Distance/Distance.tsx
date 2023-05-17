9ef72c4d8b402e57ad51fed397a9cf0cc8b46dd5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-05-17 19:31:40 +0200

    add distance component

--- src/components/Distance/Distance.tsx
+++ src/components/Distance/Distance.tsx
@@ -1,21 +1,19 @@
 /** @jsxImportSource theme-ui */
 
 type Props = {
-	distance: number | undefined | null
+	position: string
+	distance: number
 }
 
-export function Distance({ distance }: Props) {
-	if (distance) {
+export function Distance({ position, distance }: Props) {
+	if (position && distance) {
 		return (
 			<div sx={{ gridColumn: "1 / 25" }}>
-				{navigator.language.startsWith("de") ? "Entfernung: " : "Distance: "}
-				{distance.toFixed(3)} km
+				{navigator.language.startsWith("de") ? "Aktuelle Position: " : "Current Position: "}
+				<span>{position}</span>{" "}
+				({distance.toFixed(3)} km)
 			</div>
 		)
 	}
-	return (
-		<div sx={{ gridColumn: "1 / 25" }}>
-			{navigator.language.startsWith("de") ? "keine Entfernung" : "No distance"}
-		</div>
-	)
+	return (<div></div>)
 }
