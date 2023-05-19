f2667f1b4ed56a7b0e78b90f7511dd6d0b6b107a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-05-19 22:19:55 +0200

    Updates

--- static/js/113.560a93c9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkvbb=self.webpackChunkvbb||[]).push([[113],{113:function(n,e,t){t.r(e);var u=t(2791),r=t(184),i=(0,u.lazy)((function(){return t.e(282).then(t.bind(t,7282))})),s=(0,u.lazy)((function(){return Promise.all([t.e(263),t.e(985)]).then(t.bind(t,2985))}));e.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)(i,{}),(0,r.jsx)(s,{})]})}}}]);
-//# sourceMappingURL=113.560a93c9.chunk.js.map
