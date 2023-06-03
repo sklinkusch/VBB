9bb54068dae8d438ecda98af1c8a7bc8ccee7850
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-06-03 15:15:31 +0200

    remove interval for arrivals

--- src/components/TimetableArr/TimetableArr.tsx
+++ src/components/TimetableArr/TimetableArr.tsx
@@ -177,7 +177,6 @@ export default function TimetableArr() {
 			}
 		}
 		findInitialStop()
-		setInterval(() => getData(stop.id, stop.name), 120000)
 	}, [])
 	const inputField = useRef<HTMLInputElement>(null)
 	const filterField = useRef<HTMLInputElement>(null)
